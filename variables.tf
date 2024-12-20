0dbe82ac454ca602f20e59c9d78ca02004523aa1
Mikalai Yatsyna <nikolai.yatsyna@gmail.com>
2024-12-20 13:54:34 +0100

    tooling fixes
    Signed-off-by: Mikalai Yatsyna <nikolai.yatsyna@gmail.com>

--- variables.tf
+++ variables.tf
@@ -23,39 +23,5 @@ variable "template_variables" {
       values    = object({})
     }))
   })
-  default = {
-    argo_namespace = "string"
-    project_name   = "string"
-    apps = {
-      "certmanager" = {
-        name      = "cert-manager"
-        namespace = "cert-m=ns"
-        values = {
-          installCRDs = true
-          serviceAccount = {
-            name = "test_role"
-            annotations = {
-              "eks.amazonaws.com/role-arn" : "arn"
-              "eks.amazonaws.com/sts-regional-endpoints" = true
-            }
-          }
-        }
-      },
-      "ingress" = {
-        namespace = "ing-m=ns"
-        name      = "ingress"
-        values = {
-          installCRDs = true
-          serviceAccount = {
-            name = "test_role"
-            annotations = {
-              "eks.amazonaws.com/role-arn" : "arn"
-              "eks.amazonaws.com/sts-regional-endpoints" = true
-            }
-          }
-        }
-      }
-    }
-  }
   description = "Set of variables for templates"
 }
